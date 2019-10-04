4cad6d8edbef90b44a2d269e87ae4119e812799a
Tomasz Pamula <tpamula@users.noreply.github.com>
2019-10-04 16:39:21 +0100

    Updates

---
+++ precache-manifest.31a723bb9ef3525bddacf91dea89eaf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2384c00508eeaa96d7c497f0bbcad4af",
+    "url": "/tychess/index.html"
+  },
+  {
+    "revision": "16f5064fe0c67d17b796",
+    "url": "/tychess/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "905e564288990fde9ab9",
+    "url": "/tychess/static/css/main.364c80a2.chunk.css"
+  },
+  {
+    "revision": "16f5064fe0c67d17b796",
+    "url": "/tychess/static/js/2.cb3dd2d3.chunk.js"
+  },
+  {
+    "revision": "905e564288990fde9ab9",
+    "url": "/tychess/static/js/main.0aee667a.chunk.js"
+  },
+  {
+    "revision": "518072c976cd94c60560",
+    "url": "/tychess/static/js/runtime-main.3c10cb06.js"
+  }
+]);
